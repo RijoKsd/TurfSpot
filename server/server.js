f3414c84bda02fd9bb81d9d239b99a18935cd9a8
Rijo Sebastian <rijobdk@gmail.com>
2024-08-10 23:46:13 +0530

    update verel.json

--- server/server.js
+++ server/server.js
@@ -7,30 +7,9 @@ import rootRouter from "./routes/index.js";
 dotenv.config();
 
 const app = express();
-// app.use(cors());
-app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*", //backend link
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    credentials: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "Accept",
-      "Origin",
-    ],
-  })
-);
-app.options(
-  "*",
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(express.json());
+app.use(cors());
 
 // routes
 app.use("/api", rootRouter);
